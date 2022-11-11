742b3ad9a1006713e209f2fdf9e867f1a73d19b9
KayesAshfaq <asfak7074@gmail.com>
2022-11-12 02:26:22 +0600

    user location accessed on app start

--- lib/services/location.dart
+++ lib/services/location.dart
@@ -1,10 +1,53 @@
 import 'package:geolocator/geolocator.dart';
+import 'package:weather_app/utilities/constant_strings.dart';
 
 class Location {
   double? latitude;
   double? longitude;
+  String? errorMsg;
 
-  void getUserLocation() async {
+  /// Determine the current position of the device.
+  ///
+  /// When the location services are not enabled or permissions
+  /// are denied the `Future` will return an error.
+  Future<void> getUserLocation() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled request user to enable the location services.
+      errorMsg = locServiceDisabled;
+      return;
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try requesting permissions again
+        errorMsg = locPermissionDenied;
+        return;
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      errorMsg = locPermissionDeniedForever;
+      return;
+    }
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    final position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.low);
+    latitude = position.latitude;
+    longitude = position.longitude;
+    return;
+  }
+
+/*void getUserLocation() async {
     try {
       Position position = await Geolocator.getCurrentPosition(
           desiredAccuracy: LocationAccuracy.low);
@@ -15,4 +58,5 @@ class Location {
       print('location access failed!');
     }
   }
+*/
 }
---
+++ lib/utilities/constant_strings.dart
@@ -0,0 +1,4 @@
+const locServiceDisabled = 'Location services are disabled.';
+const locPermissionDenied = 'Location permissions are denied';
+const locPermissionDeniedForever =
+    'Location permissions are permanently denied, we cannot request permissions.';
